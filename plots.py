45ecf5a4a4cec9349c6cd97b2490722c4f3c3028
jana <jana.vadillo1@gmail.com>
2026-04-19 20:57:20 -0500

    added in basic programing infrastructure

---
+++ plots.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def wins_v_games():
+    #loads in a csv file into an array of net wins vs losses vs trial
+    return()
+
+def success_v_troops():
+    #loads in the averege numer of winned game after learning took place vs number of oponent troops
+    return()
+
+def Learning_rate_vs_experimental_Params():
+    #loads in the averege numer of winned game after learning took place vs number of oponent troops
+    return()
