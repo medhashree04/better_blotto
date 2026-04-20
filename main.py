cb361f9dfe7e51aedbd6a7feab99cbdb858e473c
jana <jana.vadillo1@gmail.com>
2026-04-19 23:41:35 -0500

    making sure everything is up to date

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 import numpy as np
 import random
+import pandas as pd
 
 a1_troops = 10
-a2_troops = random.randint(5, 20)
+
 fields = 3 # consistent for the game
 
+
+
+def Lotso_Choose():
+    #randomly selects an interval of five troops between five and thirty
+    troops = 5*random.randint(1,6)
+    return(troops)
+
+
+def Blotto_Choose():
+    return()
+def Define_Winner():
+    return()
+
+def Update_Q():
+    return()
+
+
+def run_experiment():
+    return()
 def main():
+    
+
     print("Better Blotto")
-    print(f"Player 2: {a2_troops} troops")
+    # print(f"Player 2: {a2_troops} troops")
 
 main()
